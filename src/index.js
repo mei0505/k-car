59d4729b79abd2c084d30d2bfbac97a5075fcf4d
mei0505 <9r529crw68@privaterelay.appleid.com>
2026-05-01 02:58:29 +0900

    Debug: ensure root element is connected

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  console.error("找不到 id 為 root 的 div！請檢查 index.html");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
